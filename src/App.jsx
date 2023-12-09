8a5a09477c1af33a3a6872dc61a89d65de055237
jhaharsh03 <harshj92002@gmail.com>
2023-12-09 19:58:00 +0530

    I am adding the basic structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './style'
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+
+    <div >
+
+    </div>
+
+    </div>
+  )
+}
+
+export default App
